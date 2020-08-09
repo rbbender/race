ca5d4d4dfb6a49265b04df7e72aa752d03482f60
rbbender <gratv0201@gmail.com>
2020-08-09 11:26:58 -0700

    removed imgui test

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,88 @@
 
 
 #include <docopt/docopt.h>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
-#include <iostream>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 
-static constexpr auto USAGE =
-  R"(Naval Fate.
+constexpr unsigned wnd_width = 640;
+constexpr unsigned wnd_height = 480;
+constexpr unsigned framerate_cap = 60;
+constexpr float radius = 100.F;
 
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
+//static constexpr auto USAGE =
+//  R"(Naval Fate.
+//
+//    Usage:
+//          naval_fate ship new <name>...
+//          naval_fate ship <name> move <x> <y> [--speed=<kn>]
+//          naval_fate ship shoot <x> <y>
+//          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
+//          naval_fate (-h | --help)
+//          naval_fate --version
+// Options:
+//          -h --help     Show this screen.
+//          --version     Show version.
+//          --speed=<kn>  Speed in knots [default: 10].
+//          --moored      Moored (anchored) mine.
+//          --drifting    Drifting mine.
+//)";
 
-int main(int argc, const char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
-
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
+//  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
+//                                                             { std::next(argv), std::next(argv, argc) },
+//                                                             true,// show help if requested
+//                                                             "Naval Fate 2.0");// version string
+//
+//  for (auto const &arg : args) {
+//    std::cout << arg.first << arg.second << std::endl;
+//  }
 
 
   //Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
   fmt::print("Hello, from {}\n", "{fmt}");
+  sf::RenderWindow window(sf::VideoMode(wnd_width, wnd_height), "ImGui + SFML = <3");
+  window.setFramerateLimit(framerate_cap);
+  ImGui::SFML::Init(window);
+
+  sf::CircleShape shape(radius);
+  shape.setFillColor(sf::Color::Cyan);
+
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event{};
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
+
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+//    ImGui::ShowDemoWindow();
+
+    ImGui::Begin("Hello, world!");
+    ImGui::Button("Look at this pretty button");
+    ImGui::End();
+
+    window.clear();
+    window.draw(shape);
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  ImGui::SFML::Shutdown();
+
+  return 0;
 }
+
+
